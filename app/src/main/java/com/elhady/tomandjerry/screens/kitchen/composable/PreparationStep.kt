da24439afd807e7d3211af159c91fd7d16cc1f8c
islam elhady <i.elhady44@gmail.com>
2025-05-26 00:29:51 +0300

    edit preparation method section

--- app/src/main/java/com/elhady/tomandjerry/screens/kitchen/composable/PreparationStep.kt
+++ app/src/main/java/com/elhady/tomandjerry/screens/kitchen/composable/PreparationStep.kt
@@ -1,13 +1,11 @@
 package com.elhady.tomandjerry.screens.kitchen.composable
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -19,36 +17,60 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.elhady.tomandjerry.composable.ibmPlexSansArabic
 
 @Composable
-fun PreparationStep(number: Int, text: String) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(12.dp))
-            .background(Color(color = 0xFFF5F5F5))
-            .padding(16.dp)
+fun PreparationStep(
+    modifier: Modifier = Modifier,
+    stepNumber: String,
+    details: String,
+) {
+    Box(
+        modifier = modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
     ) {
+        Text(
+            modifier = Modifier
+                .padding(start = 15.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.White)
+                .padding(start = 26.dp)
+                .padding(vertical = 7.dp),
+            text = details,
+            color = Color(0xFF1F1F1E),
+            fontSize = 16.sp,
+            fontFamily = ibmPlexSansArabic,
+            fontWeight = FontWeight.Normal
+        )
         Box(
             modifier = Modifier
-                .size(24.dp)
-                .background(Color.White, CircleShape),
+                .size(37.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFFD0E5F0),
+                    shape = CircleShape
+                )
+                .background(Color.White)
+                .align(Alignment.CenterStart),
             contentAlignment = Alignment.Center
         ) {
             Text(
-                text = number.toString(),
-                fontWeight = FontWeight.Bold,
-                color = Color(color = 0xFF03578A)
+                text = stepNumber,
+                color = Color(0xFF03578A),
             )
         }
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(text = text, color = Color.DarkGray)
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreparationStepPreview() {
-    PreparationStep(7, "sdfdf")
+    PreparationStep(
+        stepNumber = "6",
+        details = "Wait for the spark to ignite"
+    )
 }
