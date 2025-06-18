d37d8aa73c799874d47164d74a06365fbefed01a
islam elhady <i.elhady44@gmail.com>
2025-06-19 00:36:13 +0300

    list of tom items

---
+++ app/src/main/java/com/elhady/tomandjerry/data/TomItems.kt
@@ -0,0 +1,12 @@
+package com.elhady.tomandjerry.data
+
+import com.elhady.tomandjerry.R
+
+val tomList = listOf(
+    Tom(1, "Sport Tom", "He runs 1 meter... trips over his boot.", 3, R.drawable.sport_tom),
+    Tom(2, "Tom the lover", "He loves one-sidedly... and is beaten by the other side.", 5, R.drawable.lover_tom),
+    Tom(3, "Tom the bomb", "He blows himself up before Jerry can catch him.", 10, R.drawable.bomb_tom),
+    Tom(4, "Spy Tom", "Disguises itself as a table.", 12, R.drawable.spy_tom),
+    Tom(5, "Frozen Tom", "He was chasing Jerry, he froze after the first look.", 10, R.drawable.frozen_tom),
+    Tom(6, "Sleeping Tom", "He doesn't chase anyone, he just snores in stereo.", 10, R.drawable.sleeping_tom)
+)
